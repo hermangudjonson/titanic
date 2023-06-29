3fd6ffbf2e7a64d644c29bafa4c2446e754cd272
Herman Gudjonson <herman.gudjonson@gmail.com>
2023-06-28 20:41:48 -0400

    lightgbm stage0 hpo sweep
    added optuna objective and study for lightgbm broad hpo sweep

--- titanic/lightgbm_tune.py
+++ titanic/lightgbm_tune.py
@@ -130,13 +130,72 @@ def early_stopping_grid(n_trials=21, outdir="."):
     return study
 
 def stage0_objective(trial, X, y):
+    """objective for stage0 broad parameter sweep
     """
-    """
+    lgbm_params = {
+        "objective": 'binary',
+        "n_estimators": 10000,
+        "learning_rate": 1e-3, # expect around 2k trials 2-3min
+        "num_leaves": trial.suggest_int("num_leaves", 7, 4095),
+        "min_data_in_leaf": trial.suggest_int("min_data_in_leaf", 5, 100),
+        "lambda_l1": trial.suggest_float("lambda_l1", 1e-8, 10, log=True),
+        "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 10, log=True),
+        "feature_fraction": trial.suggest_float("feature_fraction", 0.4, 1.0),
+        "bagging_fraction": trial.suggest_float("bagging_fraction", 0.4, 1.0),
+        "bagging_freq": trial.suggest_int("bagging_freq", 1, 7)
+    }
 
-def stage0():
-    """
-    """
+    sfold = RepeatedStratifiedKFold(n_splits=5, n_repeats=2, random_state=1234)
+    # this is approximately 1e-3 decay in 10k steps
+    # learning_rates = list(lgbm_params['learning_rate'] * np.float_power(1 - 7e-4, np.arange(lgbm_params['n_estimators'])))
+    
+    eval_df = model.eval_lgbm_cv(
+        X,
+        y,
+        params=lgbm_params, 
+        cv=sfold,
+        callbacks=[
+            optuna.integration.LightGBMPruningCallback(trial, 'binary_logloss', valid_name='validation'),
+            lgbm.early_stopping(20, first_metric_only=True)
+            # lgbm.reset_parameter(learning_rate=learning_rates)
+        ]
+    )
+    
+    eval_test = eval_df.loc[eval_df.eval_set == 'test',:].mean(numeric_only=True)
+    for k, v in eval_test.items():
+        trial.set_user_attr(k, v)
+    return eval_test['binary_logloss']
 
+def stage0(prune=False, n_trials=100, timeout=3600, outdir="."):
+    """run optuna lightgbm stage0 hyperparamter optimization
+    """
+    if prune:
+        pruner = optuna.pruners.MedianPruner(
+            n_startup_trials=5, n_warmup_steps=200, interval_steps=50, n_min_trials=5)
+    else:
+        pruner = optuna.pruners.NopPruner()
+    
+    sql_file = f'sqlite:///{str(utils.WORKING_DIR / outdir / "lgbm_stage0.db")}'
+
+    study = optuna.create_study(
+        storage=sql_file,
+        load_if_exists=True,
+        study_name='lgbm_stage0',
+        pruner=pruner, 
+        direction="minimize", 
+        sampler=optuna.samplers.TPESampler()
+    )
+    
+    warnings.simplefilter('ignore') # to suppress multiple callback warning
+    # pre-load data for trials
+    raw_train_df, target_ds = load_prep.raw_train()
+    study.optimize(
+        functools.partial(early_stopping_objective, X=raw_train_df, y=target_ds),
+        n_trials=n_trials,
+        timeout=timeout
+    )
+    warnings.resetwarnings()
+    return study
 
 if __name__ == "__main__":
     fire.Fire()
