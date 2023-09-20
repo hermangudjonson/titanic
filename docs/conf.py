890458899d02af5317c9a9be5299419d4eec2446
Herman Gudjonson <herman.gudjonson@gmail.com>
2023-09-20 11:58:40 -0400

    fix conf.py formatting

--- docs/conf.py
+++ docs/conf.py
@@ -6,28 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'titanic'
-copyright = '2023, HG'
-author = 'HG'
-release = '0.0.1'
+project = "titanic"
+copyright = "2023, HG"
+author = "HG"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
-    'autoapi.extension',
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "autoapi.extension",
 ]
 
-autoapi_dirs = ['../titanic']
+autoapi_dirs = ["../titanic"]
 autoapi_add_toctree_entry = False
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
